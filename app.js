64347817b181eeb32040dad922f2c29f79b2bb5b
Pandey <Aditya.Pandey@ellucian.com>
2021-06-05 17:59:10 +0530

    Added link for poster

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ $(document).ready(()=>{
             url:url+"&t="+movie,
             success:function(data){
                 console.log(data);
+				//console.log(data.Poster);
+				//document.write();
 
 
                 result=`
-                    <img style="float:left" width=200 height=190 src="$(data.Poster)"/>
+                    <img style="float:left" width=200 height=190 src="${data.Poster}"/>
                     <h2>Title:${data.Title}</h2>
                     <h2>Release Date:${data.Released}</h2>
                     <h2>Duration:${data.Runtime}</h2>
@@ -26,6 +28,7 @@ $(document).ready(()=>{
                     <h2>Cast:${data.Actors}</h2>
                 `;
                  $("#movie").html(result);
+				 
 
             }
 
